0fa4cfd201a191e672ce32b7a168bcc002e4bef0
Isaac00rz <Isaac_marqz@outlook.com>
2018-10-26 00:47:10 -0500

    Curriculum de Isaac no funciona

---
+++ PastelesIsaac/build/generated-sources/ap-source-output/Datos/entity/Pasteles_.java
@@ -0,0 +1,17 @@
+package Datos.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-26T00:29:03")
+@StaticMetamodel(Pasteles.class)
+public class Pasteles_ { 
+
+    public static volatile SingularAttribute<Pasteles, String> descripcion;
+    public static volatile SingularAttribute<Pasteles, Double> precio;
+    public static volatile SingularAttribute<Pasteles, Double> costo;
+    public static volatile SingularAttribute<Pasteles, String> nombre;
+    public static volatile SingularAttribute<Pasteles, Integer> idPastel;
+
+}
